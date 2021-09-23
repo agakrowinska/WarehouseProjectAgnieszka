2aa199be2e800bf13279421a1a885bff22e6b4d9
agakrowinska <aga.krowinska@gmail.com>
2021-09-23 15:09:44 +0200

    first commit

---
+++ src/simple.java
@@ -0,0 +1,11 @@
+public class simple {
+    public static void main(String happy[]) {
+
+        String name = "Aga";
+        System.out.println(name.toUpperCase());
+    }
+
+    public static void addExclamationPoint(String)
+}
+
+
