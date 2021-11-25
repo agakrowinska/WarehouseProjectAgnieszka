490ca4739868b3011de052ec980f0b6f71939be7
agakrowinska <aga.krowinska@gmail.com>
2021-11-25 14:11:42 +0100

    final commit

--- src/main/java/intro/TheWarehouseApp.java
+++ src/main/java/intro/TheWarehouseApp.java
@@ -22,9 +22,8 @@ public class TheWarehouseApp {
       theManager.performAction(choice);
 
       // confirm to do more
-      //if (!theManager.confirm("Do you want to perform another action?")) {
+      if (!theManager.confirm("Do you want to perform another action?")) {
         theManager.quit();
-      }
       }
 
     } while (true);
--- src/main/java/intro/TheWarehouseManager.java
+++ src/main/java/intro/TheWarehouseManager.java
@@ -1,6 +1,9 @@
 package main.java.intro;
 
 import java.util.Scanner;
+import java.util.Locale;
+import static main.java.intro.Repository.WAREHOUSE1;
+import static main.java.intro.Repository.WAREHOUSE2;
 
 /**
  * Provides necessary methods to deal through the Warehouse management actions
@@ -29,16 +32,15 @@ public class TheWarehouseManager {
   /** Welcome User */
   public void welcomeUser() {
     this.seekUserName();
-    //this.greetUser();
-
-
+    this.greetUser();
   }
 
   /** Ask for user's choice of action */
   public int getUsersChoice() {
-    System.out.println("You probably came here with a purpose. Please tell me what would you like to do?");
-    for (String i: userOptions){
-    System.out.println(i);
+    int choice;
+
+    for (String option: userOptions){
+    System.out.println(option);
     }
 
     System.out.println("Please type the number below");
@@ -51,25 +53,20 @@ public class TheWarehouseManager {
 
   /** Initiate an action based on given option */
   public void performAction(int option) {
-  //if(option == 1) {
-    //System.out.println("Here is what we have in Warehouse 1: "+Arrays.toString(Repository.WAREHOUSE1));
-    //System.out.println("Here is what we have in Warehouse 2: "+Arrays.toString(Repository.WAREHOUSE2));
-  //}else if (option == 2){
-    //System.out.println("Which item you are interested in?");
-    //Scanner itemName = new Scanner(System.in);
-    //String itemInput = itemName.nextLine();
-    //for (String j: Repository.WAREHOUSE1){
-      //if(j == itemInput){
-        //int numberOfItems = numberOfItems++;
-      //}
-    //}
-
-
+    switch(option){
+      case 1:
+        this.listItemsByWarehouse();
+        break;
+      case 2:
+        this.searchItemAndPlaceOrder();
+        break;
+      case 3:
+        this.quit();
+        break;
+      default:
+        System.out.println("It is not a valid operation");
+    }
   }
-
-  }
-    //
-
 
   /**
    * Confirm an action
@@ -77,67 +74,62 @@ public class TheWarehouseManager {
    * @return action
    */
   public boolean confirm(String message) {
-    // TODO
-  //}
+    String choice;
+    do {
+      System.out.printf("%s (y/n)\n", message);
+      choice = this.reader.nextLine();
+      if (choice.length() > 0) {
+        choice = choice.trim();
+      }
+      choice = choice.toLowerCase();
 
-  /** End the application */
-  public void quit() {
-    System.out.printf("\nThank you for your visit, %s!\n", this.userName);
-    System.exit(0);
-  }
+    } while (!choice.startsWith("y") && !choice.startsWith("n"));
+    return choice.startsWith("y");
 
-  // =====================================================================================
-  // Private Methods
-  // =====================================================================================
-
-  /** Get user's name via CLI */
-  private void seekUserName() {
-    Scanner userScanner = new Scanner(System.in);
-    System.out.println("Hello! Great to see you! Please give me your name so I can help you further");
-    String userName = userScanner.nextLine();
-    greetUser(userName);
-
-
-  }
-
-  /** Print a welcome message with the given user's name */
-  private void greetUser(String givenName) {
-    System.out.println("Hey "+givenName+" thanks for being here! ");
-  }
-
-  private void listItemsByWarehouse() {
-    //
+    public void quit() {
+      System.out.printf("\nThank you for your visit, %s!\n", this.userName);
+      System.exit(0);
     }
-    // TODO here you use loops to show each item in each warehouse
-    //print "warehouse1:"
-    //listItems(Repository.WAREHOUSE1)
-    //print "warehouse2"
-    //listItems(Repository.WAREHOUSE2)
-    System.out.println("Here is what we have in Warehouse 1: ");
-  listItems(Repository.WAREHOUSE1);
-    System.out.println("Here is what we have in Warehouse 2: ");
-    listItems(Repository.WAREHOUSE2);
 
+    // =====================================================================================
+    // Private Methods
+    // =====================================================================================
 
+    /** Get user's name via CLI */
+    private void seekUserName () {
+      System.out.print("Hey!Great to see you, please tell my what is your name.");
+      this.userName = this.reader.nextLine();
+
+    }
+
+    /** Print a welcome message with the given user's name */
+    private void greetUser () {
+      System.out.printf("\nHello, %s !\n", this.userName);
+    }
+
+    private void listItemsByWarehouse () {
+      System.out.println("Here is what we have in Warehouse 1: ");
+      this.listItems(Repository.WAREHOUSE1);
+      System.out.println("Here is what we have in Warehouse 2: ");
+      this.listItems(Repository.WAREHOUSE2);
+
+    }
   }
 
   private void listItems(String[] warehouse) {
-    // TODO
-    ////loop over warehouse: inside the loop print each item
-    String[] warehouse = new String[];
-
-    int index =0;
-    while (index<warehouse.lenght()) {
-      String amount = warehouse[index];
-      index++;
+    for (String item : warehouse) {
+      System.out.printf("\n- %s\n", item);
     }
   }
 
   private void searchItemAndPlaceOrder() {
-    // TODO
-    //item = askItemToOrder();
-    //getAvaialbleAmount(item);
-    //if AvaialbleAmount > 0 then askAmountAndConfirmOrder(item, AvaialbleAmount)
+    String itemName = askItemToOrder();
+    int availableAmount = this.getAvailableAmount(itemName);
+    if(availableAmount > 0){
+      this.askAmountAndConfirmOrder(availableAmount, itemName);
+    }
+
+
   }
 
   /**
@@ -146,18 +138,9 @@ public class TheWarehouseManager {
    * @return String itemName
    */
   private String askItemToOrder() {
-    // TODO
-    // print What is the name of the item you want
-    //scan the input to see get the tem for the user input .nextLine()
-    //return the input
     System.out.println("Which item you are interested in?");
-    Scanner itemName = new Scanner(System.in);
-    String itemInput = itemName.nextLine();
-    for (String j : Repository.WAREHOUSE1) {
-      if (j == itemInput) {
-        int numberOfItems = numberOfItems++;
-      }
-    }
+    return  this.reader.nextLine();
+
   }
 
   /**
@@ -167,15 +150,57 @@ public class TheWarehouseManager {
    * @return integer availableCount
    */
  private int getAvailableAmount(String itemName) {
-    // TODO
-   //find availability in each warehouse
-   // find(item , WAREHOUSE1)
-   // find(item, WAREHOUSE2)
-   //calculate the total available amount
-   //check which warehouse hase more amount , w1 > w2, w1 < w2, w1 = w2
-   // display availability in each warehouse, with a string telling maximum or minimum also equality
-   //return the total amount
-  }
+   int count1 = find(itemName, WAREHOUSE1);
+   int count2 = find(itemName, WAREHOUSE2);
+   int availableAmount = count1+count2;
+
+   String foundInWareHouse;
+   if(count1 > 0 &&  count2 > 0){
+     foundInWareHouse = "Both Warehouses";
+   }else if( count1 > 0){
+     foundInWareHouse = "Warehouse1";
+   }else if( count2 > 0){
+     foundInWareHouse = "Warehouse2";
+   }else{
+     foundInWareHouse = "Not in stock";
+   }
+   int maximumQuantity;
+   String maximumQuantityIn;
+
+   if(foundInWareHouse.equals("Not in stock")){
+     maximumQuantity = 0;
+     maximumQuantityIn = "Not in stock";
+   }else if( count1 > count2 ){
+     //there are more quantity of item in warehouse 1
+     maximumQuantity = count1;
+     maximumQuantityIn = "Warehouse 1";
+   }else if(count2 > count1){
+     //there are more quantity of item in warehouse 2
+     maximumQuantity = count2;
+     maximumQuantityIn = "Warehouse 2";
+   }else {
+     maximumQuantity = count2; // or count 1 because they are equal
+     maximumQuantityIn = "Both warehouses have the same Quantity";
+   }
+
+   //display the availability details
+   //The total amount
+   System.out.printf("Amount available: %d\n",availableAmount);
+   //The location of those items : foundInWareHouse
+   System.out.printf("Location: %s\n",foundInWareHouse);
+   //the maximum availability of those items.
+   if(foundInWareHouse.equals("Not in stock")){
+     System.out.printf("Maximum availability is:0 and  %s is Not in stock",itemName);
+   }else if(count1 == count2){
+     System.out.printf("Maximum availability is: %d and Both warehouses have the same availability", count1);
+   }else{
+     System.out.printf("Maximum availability is: %d in %s\n", maximumQuantity, maximumQuantityIn);
+   }
+
+   //return total available amount
+   return availableAmount;
+ }
+
 
   /**
    * Find the count of an item in a given warehouse
@@ -185,15 +210,28 @@ public class TheWarehouseManager {
    * @return count
    */
  private int find(String item, String[] warehouse) {
-    // TODO
+   int count = 0;
+   for (String wItem :warehouse ) {
+     if(item.equals(wItem)){
+       count++;
+     }
+   }
+   return count;
   }
 
   /** Ask order amount and confirm order */
   private void askAmountAndConfirmOrder(int availableAmount, String item) {
-    // TODO
-    //ask the user how many they want
-    // if they want more than availableAmount then tell itz is not possible and ask them to choose wehter to contionue with the available amount or cancle
-    //else show the message to confirm the order
+    boolean toOrder = this.confirm("Would you like to order this item?");
+    if(toOrder){
+      //If the answer is yes, it should ask the user how many do they want
+      int orderAmount = this.getOrderAmount(availableAmount);
+
+      //if the orderAmount is -1 then just go to the searchItemAndPlaceOrder()  :
+      if(orderAmount > 0){
+        System.out.printf("%d %s %s been ordered",orderAmount,item,(orderAmount ==1 ? "has" : "have"));
+      }
+
+    }
   }
 
   /**
@@ -203,6 +241,42 @@ public class TheWarehouseManager {
    * @return
    */
   private int getOrderAmount(int availableAmount) {
-    // TODO
+    int orderAmount = -1;
+    System.out.println("What amount would you like to have?");
+    do{
+      // read the amount from the cli
+      orderAmount = Integer.parseInt(this.reader.nextLine());
+      // if the orderAmount is more than the availableAmount
+      if(orderAmount > availableAmount){
+        // it should show an error message and should ask
+
+        System.out.println("***************************************************************");
+        System.out.println("There are not many available. The maximum amount that can be ordered is "+ availableAmount);
+        System.out.println("***************************************************************");
+
+        boolean orderAll = this.confirm("Would you like to order the maximum available?");
+
+        if(orderAll){
+          orderAmount = availableAmount;
+        }else{
+          boolean keepOrdering = this.confirm("Do you want to order another amount of this item?");
+          if(keepOrdering){
+            orderAmount = -1;
+          }else{
+            orderAmount = 0;
+          }
+
+        }
+
+
+      }else if(orderAmount < 0){
+
+        System.out.println("It is not the right amount. Please put the number more than 0.");
+      }else{
+        return -1;
+      }
+    }while(orderAmount < 0 || orderAmount > availableAmount);
+
+    return orderAmount;
   }
 }
